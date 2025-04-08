d5b6eceb30188e5cfbda25af692278b9744d8f91
Alessandro Umbrico <umbrico.alessandro@gmail.com>
2025-04-08 18:16:05 +0200

    fix style

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://anacleto85.github.io',  // Replace with your GitHub username
-  base: '/bear-workshop',  // Replace with your repository name
+  base: '/bear-workshop',               // Replace with your repository name
   integrations: [tailwind()],
-  output: 'static'
+  output: 'static',
+  build: {
+    assets: 'assets'
+  }
 });
