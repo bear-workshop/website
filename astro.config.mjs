43e8b110703ebd13532efe359854655fe7f1bffb
Alessandro Umbrico <umbrico.alessandro@gmail.com>
2025-04-08 19:14:49 +0200

    Add React integration and fix Sonner Toaster component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
 export default defineConfig({
   site: 'https://bear-workshop.github.io',
   base: '/website',
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
   output: 'static',
   build: {
     assets: 'assets'
