96b29fd901a4a3ab8f0b00f6520d6e318eb16727
Alessandro Umbrico <umbrico.alessandro@gmail.com>
2025-04-08 18:44:12 +0200

    Update configuration for organization GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://anacleto85.github.io',  // Replace with your GitHub username
-  base: '/bear-workshop',               // Replace with your repository name
+  site: 'https://bear-workshop.github.io',
+  base: '',
   integrations: [tailwind()],
   output: 'static',
   build: {
